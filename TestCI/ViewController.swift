55406bbb830ce58082fcc0b05b4da1c23a8d22c8
Denis Kreshikhin <denis@kreshikhin.com>
2020-05-31 20:38:30 +0300

    Init commit

---
+++ TestCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCI
+//
+//  Created by Denis Kreshikhin on 31.05.2020.
+//  Copyright © 2020 Denis Kreshikhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
